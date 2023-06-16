c8f51fac02cef1b92a0130523a752c2795742214
ypeiyu <yangpeiyv@gmail.com>
2023-06-16 22:02:04 +0800

    first commit

--- utils/settings.py
+++ utils/settings.py
@@ -1,1 +1,18 @@
 img_size = 224
+
+parser_choices = {
+    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IntGradSG', 'IntGradSQ', 'IG_uniform', 'AGI', 'FullGrad',
+                    'SmoothGrad', 'Random'],
+    'model': ['resnet34', 'vgg16', 'MLP'],
+    'dataset': ['ImageNet', 'CIFAR-10', 'CIFAT-100', 'MNIST'],
+    'metric': ['pixel_perturb', 'sanity_check'],
+}
+
+parser_default = {
+    'attr_method': 'IG_Uniform',
+    'model': 'resnet34',
+    'dataset': 'ImageNet',
+    'metric': 'pixel_perturb',
+    'k': 5,
+    'bg_size': 10,
+}
