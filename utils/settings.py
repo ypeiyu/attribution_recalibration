c1c16592008f57fdeb492bbb3fbc867111edb449
ypeiyu <yangpeiyv@gmail.com>
2023-06-22 12:07:35 +0800

    update settings

--- utils/settings.py
+++ utils/settings.py
@@ -1,18 +1,22 @@
 img_size = 224
 
 parser_choices = {
-    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IntGradSG', 'IntGradSQ', 'IG_uniform', 'AGI', 'FullGrad',
+    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IG_SG', 'IG_SQ', 'IG_uniform', 'AGI', 'FullGrad',
                     'SmoothGrad', 'Random'],
     'model': ['resnet34', 'vgg16', 'MLP'],
     'dataset': ['ImageNet', 'CIFAR-10', 'CIFAT-100', 'MNIST'],
     'metric': ['pixel_perturb', 'sanity_check'],
+    'est_method': ['vanilla', 'valid_ip', 'valid_ref'],
+    'exp_obj': ['logit', 'prob', 'contrast'],
 }
 
 parser_default = {
-    'attr_method': 'IG_Uniform',
+    'attr_method': 'IG_SG',
     'model': 'resnet34',
     'dataset': 'ImageNet',
-    'metric': 'visualize',
+    'metric': 'DiffID',
     'k': 5,
-    'bg_size': 10,
+    'bg_size': 20,
+    'est_method': 'valid_ip',
+    'exp_obj': 'logit',
 }
