8cc6256d521d996a31fa72bb5a70313c69dea811
ypeiyu <yangpeiyv@gmail.com>
2023-06-25 11:17:08 +0800

    add tqdm

--- evaluator.py
+++ evaluator.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from tqdm import tqdm
 
 from utils import undo_preprocess_input_function
 from utils import visualize
@@ -63,7 +64,7 @@ class Evaluator(object):
                                     [torch.zeros(len(self.dataloader.dataset.samples)) for _ in range(ratio_len)]]
 
         start = time.time()
-        for batch_num, (batch_image, label) in enumerate(self.dataloader):
+        for batch_num, (batch_image, label) in enumerate(tqdm(self.dataloader)):
             batch_image = batch_image.cuda()
             target = label.cuda()
 
