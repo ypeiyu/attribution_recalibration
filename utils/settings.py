9af7819cb4d5a6ff80d101dd1683b399dd8df3c7
ypeiyu <yangpeiyv@gmail.com>
2023-07-13 22:50:00 +0800

    update preprocess

--- utils/settings.py
+++ utils/settings.py
@@ -2,21 +2,22 @@ img_size = 224
 
 parser_choices = {
     'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IG_SG', 'IG_SQ', 'IG_uniform', 'AGI', 'FullGrad',
-                    'SmoothGrad', 'Random'],
+                    'SmoothGrad', 'Random', 'MeaningfulMask'],
     'model': ['resnet34', 'vgg16', 'MLP'],
-    'dataset': ['ImageNet', 'CIFAR-10', 'CIFAT-100', 'MNIST'],
+    'dataset': ['imagenet', 'cifar10', 'cifar100', 'mnist'],
     'metric': ['DiffID', 'visualize', 'sensitivity_n', 'sanity_check'],
     'est_method': ['vanilla', 'valid_ip', 'valid_ref'],
     'exp_obj': ['logit', 'prob', 'contrast'],
 }
 
 parser_default = {
-    'attr_method': 'IG_SG',  # IG_Uniform
+    'batch_size': 64,
+    'attr_method': 'IG_Uniform',  #
     'model': 'resnet34',
-    'dataset': 'ImageNet',
+    'dataset': 'imagenet',
     'metric': 'visualize',
     'k': 5,
-    'bg_size': 20,
-    'est_method': 'vanilla',
-    'exp_obj': 'contrast',
+    'bg_size': 10,
+    'est_method': 'valid_ref',
+    'exp_obj': 'logit',
 }
