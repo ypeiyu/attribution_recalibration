1f3f627a782dd625faabb1e69ba7f9c7b089e255
ypeiyu <yangpeiyv@gmail.com>
2023-07-15 10:56:36 +0800

    update args

--- utils/settings.py
+++ utils/settings.py
@@ -1,8 +1,12 @@
-img_size = 224
+img_size_dict = {
+    'imagenet': (3, 224, 224),
+    'cifar10': (3, 32, 32),
+    'cifar100': (3, 32, 32),
+}
 
 parser_choices = {
-    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IG_SG', 'IG_SQ', 'IG_uniform', 'AGI', 'FullGrad',
-                    'SmoothGrad', 'Random', 'MeaningfulMask'],
+    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'IG_SG', 'IG_SQ', 'IG_Uniform', 'AGI', 'FullGrad',
+                    'SmoothGrad', 'Random'],
     'model': ['resnet34', 'vgg16', 'MLP'],
     'dataset': ['imagenet', 'cifar10', 'cifar100', 'mnist'],
     'metric': ['DiffID', 'visualize', 'sensitivity_n', 'sanity_check'],
@@ -18,6 +22,6 @@ parser_default = {
     'metric': 'visualize',
     'k': 5,
     'bg_size': 10,
-    'est_method': 'valid_ref',
+    'est_method': 'valid_ip',
     'exp_obj': 'logit',
 }
